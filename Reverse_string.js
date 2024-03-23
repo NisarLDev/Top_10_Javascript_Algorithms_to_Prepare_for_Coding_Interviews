7035a0fba53949f450411fce095cf117809a0c47
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-03-24 00:03:36 +0100

    Update Reverse_string.js

--- Reverse_string.js
+++ Reverse_string.js
@@ -1,1 +1,8 @@
+// ----- Directions
+// Given a string, return a new string with the reversed ordeer of characters
+// ----- Examples
+//   reverse('hi') === 'ih'
+//   reverse('hello') === 'olleh'
+//   reverse('CodingMoney') === 'yenoMgnidoC'
 
+function reverse(str)
