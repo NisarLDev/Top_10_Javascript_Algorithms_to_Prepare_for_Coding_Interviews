a62c9f4355c7bfeb9df00b08db030fa2dc22dd37
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-03-24 06:22:52 +0100

    Updated

--- Maxime_of_Characters_In_The_String/Maxime_Of_Characters_In_The_String.js
+++ Maxime_of_Characters_In_The_String/Maxime_Of_Characters_In_The_String.js
@@ -1,13 +1,24 @@
-function maxChar(str){
-    const charMap = {};
-    for(let char of str){
-        if(charMap[char]){
-            charMap[char] = charMap[char] + 1
-        }else{let char:any
-            charMap[char] = 1
-        };
-    };
-    return charMap;
-}
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
 
-console.log(maxChar("abcccccccd"));
+function maxChar(str) {
+    const charMap = {}
+    let max = 0
+    let maxChar = ''
+    for(let char of str){
+        charMap[char] = ++charMap[char] || 1
+    }
+    for(let key in charMap){
+      if(charMap[key] > max){
+        max = charMap[key]
+        maxChar = key
+      }
+    }
+    return maxChar;
+  }
+  
+  console.log(maxChar("apple 1231111"));
