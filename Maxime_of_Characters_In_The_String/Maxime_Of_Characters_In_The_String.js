d07797dd8590ac12a5e264fce721b844b524fa15
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-03-24 01:54:03 +0100

    Updated

---
+++ Maxime_of_Characters_In_The_String/Maxime_Of_Characters_In_The_String.js
@@ -0,0 +1,13 @@
+function maxChar(str){
+    const charMap = {};
+    for(let char of str){
+        if(charMap[str]){
+            charMap[char] = charMap[char] + 1
+        }else{let char:any
+            charMap[char] = 1
+        };
+    };
+    return charMap;
+}
+
+console.log(maxChar("abcccccccd"));
